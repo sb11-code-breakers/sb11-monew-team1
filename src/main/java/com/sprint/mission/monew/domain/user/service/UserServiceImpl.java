4f460b4488080305b8cce685ff244db64eb03052
wuuujuuun <wuuujuuun@gmail.com>
2026-05-27 16:48:08 +0900

    feat(green): UserService register 메서드 구현

--- src/main/java/com/sprint/mission/monew/domain/user/service/UserServiceImpl.java
+++ src/main/java/com/sprint/mission/monew/domain/user/service/UserServiceImpl.java
@@ -2,7 +2,12 @@ package com.sprint.mission.monew.domain.user.service;
 
 import com.sprint.mission.monew.domain.user.dto.UserDto;
 import com.sprint.mission.monew.domain.user.dto.UserRegisterRequest;
+import com.sprint.mission.monew.domain.user.entity.User;
+import com.sprint.mission.monew.domain.user.exception.UserEmailDuplicateException;
+import com.sprint.mission.monew.domain.user.mapper.UserMapper;
+import com.sprint.mission.monew.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,8 +16,24 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class UserServiceImpl implements UserService {
 
+  private final UserRepository userRepository;
+  private final UserMapper userMapper;
+  private final PasswordEncoder passwordEncoder;
+
+  @Transactional
   @Override
   public UserDto register(UserRegisterRequest request) {
-    return null;
+    if (userRepository.existsByEmail(request.email())) {
+      throw UserEmailDuplicateException.withEmail(request.email());
+    }
+
+    User user = User.create(
+        request.email(),
+        request.nickname(),
+        passwordEncoder.encode(request.password())
+    );
+
+    User saved = userRepository.save(user);
+    return userMapper.toDto(saved);
   }
 }
