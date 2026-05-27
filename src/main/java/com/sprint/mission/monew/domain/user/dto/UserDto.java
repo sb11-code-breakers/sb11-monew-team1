28efbf1224903c9b5ae0544e58fa73724c0b904e
wuuujuuun <wuuujuuun@gmail.com>
2026-05-27 16:19:26 +0900

    feat: User DTO 작성 (UserRegisterRequest, UserDto)

---
+++ src/main/java/com/sprint/mission/monew/domain/user/dto/UserDto.java
@@ -0,0 +1,13 @@
+package com.sprint.mission.monew.domain.user.dto;
+
+import java.time.Instant;
+import java.util.UUID;
+
+public record UserDto(
+    UUID id,
+    String email,
+    String nickname,
+    Instant createdAt
+) {
+
+}
