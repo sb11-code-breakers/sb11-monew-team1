00dc9fc6d3dcaee11f47467c6cc776d9595d590f
wuuujuuun <wuuujuuun@gmail.com>
2026-05-28 17:52:25 +0900

    feat: UserUpdateRequest DTO 작성

---
+++ src/main/java/com/sprint/mission/monew/domain/user/dto/UserUpdateRequest.java
@@ -0,0 +1,10 @@
+package com.sprint.mission.monew.domain.user.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record UserUpdateRequest(
+    @NotBlank @Size(min = 2, max = 20) String nickname
+) {
+
+}
