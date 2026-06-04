fbbcb3937fda719202c736d4dee50be6a864a22a
wuuujuuun <wuuujuuun@gmail.com>
2026-06-04 15:27:04 +0900

    fix: 비밀번호 변경 시 새 비밀번호 유효성 검사 추가 수정

--- src/main/java/com/sprint/mission/monew/domain/user/dto/UserPasswordUpdateRequest.java
+++ src/main/java/com/sprint/mission/monew/domain/user/dto/UserPasswordUpdateRequest.java
@@ -1,8 +1,13 @@
 package com.sprint.mission.monew.domain.user.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 public record UserPasswordUpdateRequest(
     @NotBlank String currentPassword,
-    @NotBlank String newPassword
+    @NotBlank
+    @Size(min = 8, max = 50)
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d).*$", message = "비밀번호는 영문자와 숫자를 포함해야 합니다")
+    String newPassword
 ) {}
