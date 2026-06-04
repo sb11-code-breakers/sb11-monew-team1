6bdbdcaaadd77c5a4ab2c28376425ba7365a4532
wuuujuuun <wuuujuuun@gmail.com>
2026-06-04 14:14:38 +0900

    test(red): EmailService 인증 이메일 발송 실패 테스트 작성

--- src/main/java/com/sprint/mission/monew/domain/user/service/EmailService.java
+++ src/main/java/com/sprint/mission/monew/domain/user/service/EmailService.java
@@ -6,7 +6,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import software.amazon.awssdk.services.ses.SesClient;
-import software.amazon.awssdk.services.ses.model.SendEmailRequest;
 
 @Slf4j
 @Service
@@ -22,29 +21,6 @@ public class EmailService {
   private String verificationBaseUrl;
 
   public boolean sendVerificationEmail(String to, String token) {
-    try {
-      String verificationUrl = verificationBaseUrl + "/api/users/verify?token=" + token;
-
-      SendEmailRequest request = SendEmailRequest.builder()
-          .destination(d -> d.toAddresses(to))
-          .message(m -> m
-              .subject(c -> c.data("[MoNew] 이메일 인증을 완료해주세요"))
-              .body(b -> b.html(c -> c.data(
-                  "<h2>MoNew 이메일 인증</h2>"
-                      + "<p>아래 버튼을 클릭하여 이메일 인증을 완료해주세요.</p>"
-                      + "<a href='" + verificationUrl + "'>이메일 인증하기</a>"
-                      + "<p>링크는 24시간 후 만료됩니다.</p>"
-              )))
-          )
-          .source(sender)
-          .build();
-
-      sesClient.sendEmail(request);
-      log.info("인증 이메일 발송 완료: to={}", MonewUtil.maskEmail(to));
-      return true;
-    } catch (Exception e) {
-      log.error("인증 이메일 발송 실패: to={}", MonewUtil.maskEmail(to), e);
-      return false;
-    }
+    return false;
   }
 }
