86fcb61d51d612c90abb2a66b0328197da466304
vincent865 <tkrhdtkfkdgo@gmail.com>
2026-06-08 18:18:46 +0900

    test(green): 전달한 값으로 RecentCommentLike를 생성한다

---
+++ src/main/java/com/sprint/mission/monew/domain/useractivity/document/RecentCommentLike.java
@@ -0,0 +1,41 @@
+package com.sprint.mission.monew.domain.useractivity.document;
+
+import java.time.Instant;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class RecentCommentLike {
+
+  private UUID id;
+  private Instant createdAt;
+  private UUID commentId;
+  private UUID articleId;
+  private String articleTitle;
+  private UUID commentUserId;
+  private String commentUserNickname;
+  private String commentContent;
+  private long commentLikeCount;
+  private Instant commentCreatedAt;
+
+  public static RecentCommentLike of(
+      UUID id, Instant createdAt, UUID commentId, UUID articleId,
+      String articleTitle, UUID commentUserId, String commentUserNickname,
+      String commentContent, long commentLikeCount, Instant commentCreatedAt) {
+    RecentCommentLike doc = new RecentCommentLike();
+    doc.id = id;
+    doc.createdAt = createdAt;
+    doc.commentId = commentId;
+    doc.articleId = articleId;
+    doc.articleTitle = articleTitle;
+    doc.commentUserId = commentUserId;
+    doc.commentUserNickname = commentUserNickname;
+    doc.commentContent = commentContent;
+    doc.commentLikeCount = commentLikeCount;
+    doc.commentCreatedAt = commentCreatedAt;
+    return doc;
+  }
+}
