abda1b2de06715d0617c6a7ff6942bd4fb955ff4
vincent865 <tkrhdtkfkdgo@gmail.com>
2026-06-15 14:34:33 +0900

    chore: merge충돌 해결

--- src/main/java/com/sprint/mission/monew/domain/article/repository/ArticleViewRepository.java
+++ src/main/java/com/sprint/mission/monew/domain/article/repository/ArticleViewRepository.java
@@ -5,9 +5,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -19,14 +17,6 @@ public interface ArticleViewRepository extends JpaRepository<ArticleView, UUID> 
   Set<UUID> findArticleIdsByArticleIdsAndUserId(
       @Param("articleIds") List<UUID> articleIds, @Param("userId") UUID userId);
 
-  @Query("SELECT av FROM ArticleView av " +
-      "JOIN FETCH av.article a " +
-      "WHERE av.userId = :userId " +
-      "AND a.deletedAt IS NULL " +
-      "ORDER BY av.createdAt DESC "
-  )
-  List<ArticleView> findTop10ByUserIdAndArticleNotDeleted(
-      @Param("userId") UUID userId, Pageable pageable);
 
   boolean existsByArticleIdAndUserId(UUID articleId, UUID userId);
 
